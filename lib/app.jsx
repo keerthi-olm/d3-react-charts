97ce21319c51629319bb9b9df6cef6d06f99e7b3
keerthi-olm <keerthi.viswanathan@olmgroup.com>
2017-02-17 17:26:13 +0000

    reactored page view so that its part of index.hyml rather than a view

--- lib/app.jsx
+++ lib/app.jsx
@@ -40,37 +40,8 @@ var Visitors = React.createClass({
     }
 });
 
-var Page = React.createClass({
-    render:function(){
 
-        return (
-            <div className="pure-g container">
-               <div className="pure-u-1">
-                                    <div className="top fontSize">
-                                        <a className="page-header" href="http://www.adeveloperdiary.com/d3-js/how-to-integrate-react-and-d3-the-right-way/"><h4 className="page-h4">(React + D3) Click here to view the full tutorial on adeveloperdiary.com</h4></a>
-                                    </div>
-                </div>
-                <div className="pure-u-1">
-                    <div className="top" id="top-line-chart">
-                        <Visitors/>
-                     </div>
-                </div>
-                <div className="pure-u-5-12">
-                     <div className="bottom-left" id="browser">
-                        <Browser/>
-                     </div>
-                </div>
-                <div className="pure-u-7-12">
-                    <div className="bottom-right" id="ret_visitors">
-                       <RetVisitors/>
-                    </div>
-                </div>
-            </div>
-        )
-    }
-});
 
-ReactDOM.render(<Page/>,document.getElementById("page"));
 ReactDOM.render(<Browser/>,document.getElementById("browser"));
 ReactDOM.render(<RetVisitors/>,document.getElementById("ret_visitors"));
 ReactDOM.render(<Visitors/>,document.getElementById("top-line-chart"));
